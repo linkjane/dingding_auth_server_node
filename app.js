3ac3ab6968afc948d5dcab9d3ae1feb3bca9ca05
link <916969890@qq.com>
2017-07-20 00:35:09 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,75 @@
+
+import Koa from 'koa';
+import Router from 'koa-router';
+import logger from 'koa-logger';
+import querystring from 'querystring';
+import fetch from 'node-fetch';
+import url from 'url';
+import crypto  from 'crypto';
+
+const router = new Router();
+const app = new Koa();
+
+const OAPI_HOST = 'https://oapi.dingtalk.com';
+
+// const { corpId, corpSecret } = require('./env');
+
+import env from './env.js';
+const { corpid, corpsecret } = env;
+
+app.use(logger());
+
+router.get('/auth', async (ctx, next) => {
+
+  let noceStr = 'abcdefg';
+  let timeStamp = Date.now();
+  //获得 请求url
+  let singedUrl = decodeURIComponent(ctx.request.href);
+
+  let initConfig = {
+    method: 'GET',
+  };
+
+  //获得访问钥匙  
+  let accessQs = querystring.stringify({
+    corpid,
+    corpsecret
+  });
+  let res = await fetch(`${OAPI_HOST}/gettoken?${accessQs}`);
+  res = await res.json();
+  
+  const access_token = res.access_token;
+
+  //获得门票  
+  let jsapiTicketQs = querystring.stringify({
+    type: 'jsapi',
+    'access_token': access_token
+  })
+  res = await fetch(`${OAPI_HOST}/get_jsapi_ticket?${jsapiTicketQs}`);
+  res = await res.json();
+
+  const ticket = res.ticket;
+  let urlObj = url.parse(singedUrl);
+  delete urlObj['hash'];
+
+  let newUrl = url.format(urlObj);
+
+
+  const str = `jsapi_ticket=${ticket}&noncestr=${noceStr}&timestamp=${timeStamp}$url=${newUrl}`;
+
+  let sha1 = crypto.createHash('sha1');
+  sha1.update(str, 'utf8');
+  let signature = sha1.digest('hex');
+  ctx.body = {
+    signature,
+    noceStr,
+    timeStamp,
+    corpId
+  };
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3004);
---
+++ env.js
@@ -0,0 +1,4 @@
+export default {
+    corpid: 'ding8770e1004a278de3',
+    corpsecret: 'TtWySLQE2reESnIZM46CO_wOWCAWGfM45GDsT0SgOKSSsQEQKUSIQBw5VCwnDayz'
+}
